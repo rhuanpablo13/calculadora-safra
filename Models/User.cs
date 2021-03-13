43a08a8d055d336930cc26149efefdf150642dd2
Andressa <isa.desenvolvimento@gmail.com>
2021-03-13 16:10:18 -0300

    auth impl

--- Models/User.cs
+++ Models/User.cs
@@ -1,16 +1,35 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
 
 namespace calculadora_api.Models
 {
-    public class User
+    public class User : IUser
     {
+        private readonly IHttpContextAccessor _accessor;
+
+        public User(IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
+
+
         [Key]
         public int Id { get; set; }
-        public string Password { get; set; }
-        public string Username { get; set; }
         public DateTime CreatedDate { get; set; }
         public string Profile { get; set; }
-        public Boolean Status { get; set; }
+
+        public string Name => _accessor.HttpContext.User.Identity.Name;
+
+        public bool IsAuthenticated() {
+            return _accessor.HttpContext.User.Identity.IsAuthenticated;
+        }
+
+        public IEnumerable<Claim> GetClaimsIdentity() {
+            return  _accessor.HttpContext.User.Claims;
+        }
+
     }
 }
